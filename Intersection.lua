88a00930e204662061f0b58ad76ec78f7069b027
Trollapse <60831236+Trollapse@users.noreply.github.com>
2024-04-11 11:26:03 -0500

    Create Intersection.lua

---
+++ Intersection.lua
@@ -0,0 +1,11 @@
+local Intersection = {}
+--[[
+	Intersection::CapsuleToTriangle()
+	Parameters: CapsuleObject, TriangleObject
+	Returns: Intersection data between the capsule/triangle -> Position, Normal, Penetration
+]]
+function Intersection.CapsuleToTriangle(CapsuleObject, TriangleObject)
+	
+end
+
+return Intersection
