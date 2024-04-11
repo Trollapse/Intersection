1f2a346a8ccaeacea5633d5b25dae3bdfe2cb835
Trollapse <60831236+Trollapse@users.noreply.github.com>
2024-04-11 11:34:26 -0500

    Update CapsuleObject.lua

--- CapsuleObject.lua
+++ CapsuleObject.lua
@@ -8,4 +8,12 @@ function CapsuleObject.new(radius, height)
 	return self
 end
 
+function CapsuleObject.GetPoints()
+	return 
+		self.Position,
+		self.Position + Vector3.new(0, self.Height, 0),
+		self.Position + Vector3.new(0, self.Radius, 0),
+		self.Position + Vector3.new(0, self.Height - self.Radius, 0)
+end
+
 return CapsuleObject
